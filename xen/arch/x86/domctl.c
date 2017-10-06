1db66cd0677c20b470fb56809ff30a47cfe69ac4
Yi Sun <yi.y.sun@linux.intel.com>
2017-10-06 19:25:59 +0800

    Rename PSR sysctl/domctl interfaces and xsm policy to make them be general
    This patch renames PSR sysctl/domctl interfaces and related xsm policy to
    make them be general for all resource allocation features but not only
    for CAT. Then, we can resuse the interfaces for all allocation features.
    
    Basically, it changes 'psr_cat_op' to 'psr_alloc', and remove 'CAT_' from some
    macros. E.g.:
    1. psr_cat_op -> psr_alloc
    2. XEN_DOMCTL_psr_cat_op -> XEN_DOMCTL_psr_alloc
    3. XEN_SYSCTL_psr_cat_op -> XEN_SYSCTL_psr_alloc
    4. XEN_DOMCTL_PSR_CAT_SET_L3_CBM -> XEN_DOMCTL_PSR_SET_L3_CBM
    5. XEN_SYSCTL_PSR_CAT_get_l3_info -> XEN_SYSCTL_PSR_get_l3_info
    
    Signed-off-by: Yi Sun <yi.y.sun@linux.intel.com>
    Reviewed-by: Wei Liu <wei.liu2@citrix.com>
    Reviewed-by: Roger Pau Monné <roger.pau@citrix.com>
    ---
    CC: Jan Beulich <jbeulich@suse.com>
    CC: Andrew Cooper <andrew.cooper3@citrix.com>
    CC: Wei Liu <wei.liu2@citrix.com>
    CC: Ian Jackson <ian.jackson@eu.citrix.com>
    CC: Daniel De Graaf <dgdegra@tycho.nsa.gov>
    CC: Roger Pau Monné <roger.pau@citrix.com>
    CC: Chao Peng <chao.p.peng@linux.intel.com>
    
    v6:
        - move macro definition into the function and undefine it after use.
          (suggested by Roger Pau Monné)
        - do not bump sysctl version because it has been bumped for 4.10.
          (suggested by Roger Pau Monné)
    v5:
        - remove domctl version number upgrade.
          (suggested by Jan Beulich)
        - restore 'XEN_SYSCTL_PSR_CAT_L3_CDP'.
          (suggested by Jan Beulich)
        - define a local macro to complete psr get value flow.
          (suggested by Roger Pau Monné)
        - remove 'Reviewed-by' and 'Acked-by'.
          (suggested by Wei Liu)
    v4:
        - remove 'ALLOC_' from names.
          (suggested by Roger Pau Monné)
        - fix comments.
          (suggested by Roger Pau Monné)
    v3:
        - remove 'op/OP' from names and modify some names from 'PSR_CAT' to
          'PSR_ALLOC'.
          (suggested by Roger Pau Monné)
    v1:
        - add description about what to be changed in commit message.
          (suggested by Wei Liu)
        - bump sysctl/domctl version numbers.
          (suggested by Wei Liu)

--- xen/arch/x86/domctl.c
+++ xen/arch/x86/domctl.c
@@ -1438,67 +1438,66 @@ long arch_do_domctl(
         }
         break;
 
-    case XEN_DOMCTL_psr_cat_op:
-        switch ( domctl->u.psr_cat_op.cmd )
-        {
-            uint32_t val32;
+    case XEN_DOMCTL_psr_alloc:
+#define domctl_psr_get_val(d, domctl, type, copyback) ({   \
+    uint32_t v;                                            \
+    int r = psr_get_val(d, domctl->u.psr_alloc.target,     \
+                        &v, type);                         \
+                                                           \
+    domctl->u.psr_alloc.data = v;                          \
+    copyback = true;                                       \
+    r;                                                     \
+})
 
-        case XEN_DOMCTL_PSR_CAT_OP_SET_L3_CBM:
-            ret = psr_set_val(d, domctl->u.psr_cat_op.target,
-                              domctl->u.psr_cat_op.data,
+        switch ( domctl->u.psr_alloc.cmd )
+        {
+        case XEN_DOMCTL_PSR_SET_L3_CBM:
+            ret = psr_set_val(d, domctl->u.psr_alloc.target,
+                              domctl->u.psr_alloc.data,
                               PSR_CBM_TYPE_L3);
             break;
 
-        case XEN_DOMCTL_PSR_CAT_OP_SET_L3_CODE:
-            ret = psr_set_val(d, domctl->u.psr_cat_op.target,
-                              domctl->u.psr_cat_op.data,
+        case XEN_DOMCTL_PSR_SET_L3_CODE:
+            ret = psr_set_val(d, domctl->u.psr_alloc.target,
+                              domctl->u.psr_alloc.data,
                               PSR_CBM_TYPE_L3_CODE);
             break;
 
-        case XEN_DOMCTL_PSR_CAT_OP_SET_L3_DATA:
-            ret = psr_set_val(d, domctl->u.psr_cat_op.target,
-                              domctl->u.psr_cat_op.data,
+        case XEN_DOMCTL_PSR_SET_L3_DATA:
+            ret = psr_set_val(d, domctl->u.psr_alloc.target,
+                              domctl->u.psr_alloc.data,
                               PSR_CBM_TYPE_L3_DATA);
             break;
 
-        case XEN_DOMCTL_PSR_CAT_OP_SET_L2_CBM:
-            ret = psr_set_val(d, domctl->u.psr_cat_op.target,
-                              domctl->u.psr_cat_op.data,
+        case XEN_DOMCTL_PSR_SET_L2_CBM:
+            ret = psr_set_val(d, domctl->u.psr_alloc.target,
+                              domctl->u.psr_alloc.data,
                               PSR_CBM_TYPE_L2);
             break;
 
-        case XEN_DOMCTL_PSR_CAT_OP_GET_L3_CBM:
-            ret = psr_get_val(d, domctl->u.psr_cat_op.target,
-                              &val32, PSR_CBM_TYPE_L3);
-            domctl->u.psr_cat_op.data = val32;
-            copyback = true;
+        case XEN_DOMCTL_PSR_GET_L3_CBM:
+            ret = domctl_psr_get_val(d, domctl, PSR_CBM_TYPE_L3, copyback);
             break;
 
-        case XEN_DOMCTL_PSR_CAT_OP_GET_L3_CODE:
-            ret = psr_get_val(d, domctl->u.psr_cat_op.target,
-                              &val32, PSR_CBM_TYPE_L3_CODE);
-            domctl->u.psr_cat_op.data = val32;
-            copyback = true;
+        case XEN_DOMCTL_PSR_GET_L3_CODE:
+            ret = domctl_psr_get_val(d, domctl, PSR_CBM_TYPE_L3_CODE, copyback);
             break;
 
-        case XEN_DOMCTL_PSR_CAT_OP_GET_L3_DATA:
-            ret = psr_get_val(d, domctl->u.psr_cat_op.target,
-                              &val32, PSR_CBM_TYPE_L3_DATA);
-            domctl->u.psr_cat_op.data = val32;
-            copyback = true;
+        case XEN_DOMCTL_PSR_GET_L3_DATA:
+            ret = domctl_psr_get_val(d, domctl, PSR_CBM_TYPE_L3_DATA, copyback);
             break;
 
-        case XEN_DOMCTL_PSR_CAT_OP_GET_L2_CBM:
-            ret = psr_get_val(d, domctl->u.psr_cat_op.target,
-                              &val32, PSR_CBM_TYPE_L2);
-            domctl->u.psr_cat_op.data = val32;
-            copyback = true;
+        case XEN_DOMCTL_PSR_GET_L2_CBM:
+            ret = domctl_psr_get_val(d, domctl, PSR_CBM_TYPE_L2, copyback);
             break;
 
         default:
             ret = -EOPNOTSUPP;
             break;
         }
+
+#undef domctl_psr_get_val
+
         break;
 
     case XEN_DOMCTL_disable_migrate:
